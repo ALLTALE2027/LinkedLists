81898e4f43e681383590ec651bec9c7efd7a6952
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-24 17:06:55 +0530

    [Darshan]: Add uc-2 add a nodes to  a linked list 70,30,56

--- LinkedLists/Program.cs
+++ LinkedLists/Program.cs
@@ -7,11 +7,18 @@
             Console.WriteLine("Linked List!!!");
             var linkedList=new LinkedList();
 
-            Console.WriteLine("Press 1 To create a linked List");
+            Console.WriteLine("1. To create a linked List");
+            Console.WriteLine("2. Add node from Head");
             int input = int.Parse(Console.ReadLine());
             switch (input)
             {
                 case 1:
+                    linkedList.AddNode(70);
+                    linkedList.AddNode(30);
+                    linkedList.AddNode(56);
+                    linkedList.DisplayNodes();
+                    break;
+                case 2:
                     linkedList.AddNode(70);
                     linkedList.AddNode(30);
                     linkedList.AddNode(56);
