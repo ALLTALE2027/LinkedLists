a2a2066504b71c585fe4c55fe1a6fd93318d15c9
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-24 17:00:35 +0530

    [Darshan]: Add uc-1 create a linked list 56->30->70

---
+++ LinkedLists/LinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class LinkedList
+    {
+        private Node Head;
+        private Node Tail;
+        private int size = 0;  // contains size of our LinkedList
+
+        public void AddNode(int data)   // this adds node at start 
+        {
+            Node node=new Node(data);
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+            size++;
+        }
+
+        public void AppendNode(int data)
+        {
+            Node node = new Node(data);
+
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Tail.next = node;
+                Tail = node;
+            }
+            size++;
+        }
+
+
+        public void InsertNode(int data, int afterNum)
+        {
+            Node node=new Node(data);
+            Node temp = Head;
+
+            while (temp.data!=afterNum)
+            {
+                temp = temp.next;
+            }
+
+            node.next = temp.next;
+            temp.next = node;
+            size++;
+            Console.WriteLine($"Node inserted after {0}",afterNum);
+
+        }
+
+
+        public void DeleteFirstNode()
+        {
+            int value = Head.data;
+            Head = Head.next;
+
+            if (Head == null)
+                Tail = null;
+
+            size--;
+            Console.WriteLine("Node with data {0} was deleted", value);
+        }
+
+        public void DeleteLastNode()
+        {
+            Node temp = Head;
+            Node current = null;  
+
+            while (temp.next != null)
+            {
+                current = temp;
+                temp = temp.next;
+            }
+
+            Tail = current;
+            Tail.next = null;
+            size--;
+            Console.WriteLine("Last node deleted");
+        }
+
+        public void DeleteIntermediateNode(int data)
+        {
+            if (Head == null)
+                Console.WriteLine("No element is present");
+
+            else
+            {
+                Node temp = Head;
+
+                while (temp != null)
+                {
+                    if (temp.next.data==data)
+                    {
+                        temp.next=temp.next.next;
+                        Console.WriteLine("Node is deleted");
+                        break;
+                    }
+                    temp=temp.next;
+                }
+                size--;
+            }
+        }
+
+        public void DisplayNodes()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+            {
+                Console.WriteLine("Linked list contents");
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    if (temp.next != null)
+                        Console.Write("->");
+                    temp = temp.next;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("LinkedList Size is: " + size);
+            }
+        }
+
+    }
+    }
---
+++ LinkedLists/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List!!!");
+            var linkedList=new LinkedList();
+
+            Console.WriteLine("Press 1 To create a linked List");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    linkedList.AddNode(70);
+                    linkedList.AddNode(30);
+                    linkedList.AddNode(56);
+                    linkedList.DisplayNodes();
+                    break;
+                default:
+                    Console.WriteLine("Select a proper choice");
+                    break;
+            }
+        }
+    }
+}
